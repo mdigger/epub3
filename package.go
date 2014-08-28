3995d501b1aa11409d1d86d0c6a8104f6cce8138
Dmitry Sedykh <dmitrys@xyzrd.com>
2014-08-29 02:56:22 +0400

    Package description

--- package.go
+++ package.go
@@ -1,3 +1,10 @@
+// The EPUB specification is a distribution and interchange format standard for digital
+// publications and documents. EPUB defines a means of representing, packaging and encoding
+// structured and semantically enhanced Web content — including HTML5, CSS, SVG, images, and other
+// resources — for distribution in a single-file format.
+//
+// This library describes the format of the data used in the epub, and offers easy writer to create
+// publications in this format.
 package epub
 
 import (
