b361240265398b66e2732106dd0d2d03559c1cba
Dmitry Sedykh <dmitrys@xyzrd.com>
2014-08-29 04:18:32 +0400

    Комментарии к контейнеру

--- container.go
+++ container.go
@@ -4,17 +4,20 @@ import (
 	"encoding/xml"
 )
 
+// Predefined names of folder and container file.
 const (
 	METAINF   = "META-INF"
 	CONTAINER = "container.xml"
 )
 
+// Container describes the contents of the container
 type Container struct {
 	XMLName   xml.Name    `xml:"urn:oasis:names:tc:opendocument:xmlns:container container"`
 	Version   string      `xml:"version,attr"`
 	Rootfiles []*RootFile `xml:"rootfiles>rootfile"`
 }
 
+// RootFile describes the path to description of publication.
 type RootFile struct {
 	FullPath  string `xml:"full-path,attr"`
 	MediaType string `xml:"media-type,attr"`
