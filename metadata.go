33e5e12b3e30fd56f7e677bd74be3fd0d6e1f671
Dmitry Sedykh <sedykh@gmail.com>
2021-05-16 21:24:08 +0300

    add type & source metadata

--- metadata.go
+++ metadata.go
@@ -13,16 +13,18 @@ type Metadata struct {
 	Title      []ElementLang `xml:"dc:title"`      // The [DCMES] title element represents an instance of a name given to the EPUB Publication.
 	Language   []Element     `xml:"dc:language"`   // The [DCMES] language element specifies the language of the Publication content.
 	// DCMES Optional Elements
+	Date        *Element      `xml:"dc:date,omitempty"`        // The date element must only be used to define the publication date of the EPUB Publication. The publication date is not the same as the last modified date (the last time the content was changed), which must be included using the [DCTERMS] modified property.
 	Creator     []ElementLang `xml:"dc:creator,omitempty"`     // The creator element represents the name of a person, organization, etc. responsible for the creation of the content of a Publication. The role property can be attached to the element to indicate the function the creator played in the creation of the content.
 	Contributor []ElementLang `xml:"dc:contributor,omitempty"` // The contributor element is used to represent the name of a person, organization, etc. that played a secondary role in the creation of the content of a Publication.
-	Date        *Element      `xml:"dc:date,omitempty"`        // The date element must only be used to define the publication date of the EPUB Publication. The publication date is not the same as the last modified date (the last time the content was changed), which must be included using the [DCTERMS] modified property.
-	Coverage    []ElementLang `xml:"dc:coverage,omitempty"`
+	Subject     []ElementLang `xml:"dc:subject,omitempty"`
 	Description []ElementLang `xml:"dc:description,omitempty"`
+	Type        []Element     `xml:"dc:type,omitempty"`
 	Format      []Element     `xml:"dc:format,omitempty"`
 	Publisher   []ElementLang `xml:"dc:publisher,omitempty"`
+	Source      []Element     `xml:"dc:source,omitempty"`
 	Relation    []ElementLang `xml:"dc:relation,omitempty"`
+	Coverage    []ElementLang `xml:"dc:coverage,omitempty"`
 	Rights      []ElementLang `xml:"dc:rights,omitempty"`
-	Subject     []ElementLang `xml:"dc:subject,omitempty"`
 	// Meta
 	Meta []Meta `xml:"meta,omitempty"` // The meta element provides a generic means of including package metadata, allowing the expression of primary metadata about the package or content and refinement of that metadata.
 	Link []Link `xml:"link,omitempty"` // The link element is used to associate resources with a Publication, such as metadata records.
